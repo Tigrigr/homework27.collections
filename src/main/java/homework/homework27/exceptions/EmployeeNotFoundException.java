e42adc904f2747229fdb49a3f42eb61c0e26ebf2
mikha <Mikhailov.dzenphone@gmail.com>
2023-07-02 20:45:29 +0300

    Initial commit

---
+++ src/main/java/homework/homework27/exceptions/EmployeeNotFoundException.java
@@ -0,0 +1,13 @@
+package homework.homework27.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class EmployeeNotFoundException extends RuntimeException {
+    public EmployeeNotFoundException() {
+        super("Employee Not Found");
+    }
+}
+
